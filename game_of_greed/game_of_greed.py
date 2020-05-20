d5c53fcb3226e670db52e6db6616cf2397c64ad6
Peng Chen <pchen0816@outlook.com>
2020-05-20 11:08:52 -0700

    game logic fix and passed all test

--- game_of_greed/game_of_greed.py
+++ game_of_greed/game_of_greed.py
@@ -69,20 +69,18 @@ class Banker:
 class Game:
     def __init__(self, roller=None):
         self.dice_deck = []
-        self.score = 0
         self.banker = Banker()
         self.round = 1
         self.game = GameLogic()
         self.roller = roller
 
-    def play(self):
-
-        def quit():
+    def quit_game(self):
             """function to print result when user choose to quit"""
             print(f"Total score is {self.banker.balance} points")
             print(f"Thanks for playing. You earned {self.banker.balance} points")
 
-        def cheat_checker(user_input, dice_roll):
+    @staticmethod
+    def cheat_checker(user_input, dice_roll):
             """checks whether the user is cheating or not"""
             for i in user_input:
                 if i in dice_roll:
@@ -91,57 +89,69 @@ class Game:
                     return False
             return True
 
+    def greeting(self):
+        """ask whether the user want to play the game"""
         print("Welcome to Game of Greed")
         responses = input("Wanna play?")
         if responses == 'n':
             print("OK. Maybe another time")
-        elif responses =='y':
-            """keep the game going unless we break it"""
-            while True:
+        elif responses == 'y':
+            self.game_cycle()
 
-                print(f"Starting round {self.round}")
-                print(f"Rolling {6-len(self.dice_deck)} dice...")
-                if self.roller==None:
-                    rolls = self.game.roll_dice(6-len(self.dice_deck))
-                else:
-                    rolls = self.roller
-                print(",".join([str(i) for i in rolls]))
-                score_check = self.game.calculate_score(rolls)
-                if score_check != 0:
-                    dice_to_save = input("Enter dice to keep (no spaces), or (q)uit: ")
-                    if dice_to_save != 'q':
-                        dice_check = [int(elem) for elem in list(dice_to_save)]
-                        result_check = cheat_checker(dice_check,list(rolls))
-                        while result_check == False and dice_to_save != 'q':
-                            print("Cheater!!! Or possibly made a typo...")
-                            print(",".join([str(i) for i in rolls]))
-                            dice_to_save = input("Enter dice to keep (no spaces), or (q)uit: ")
-                            if dice_to_save != 'q':
-                                dice_check = [int(elem) for elem in list(dice_to_save)]
-                                result_check = cheat_checker(dice_check,list(rolls))
-                else:
-                    quit()
-                    break
 
+    def show_shelf_score(self,user_choice):
+        """display the scores on shelf and print to console"""
+        dice_for_score =[]
+        for i in range(len(user_choice)):
+            self.dice_deck.append(int(user_choice[i]))
+            dice_for_score.append(int(user_choice[i]))
+
+        new_score = self.game.calculate_score(dice_for_score)
+
+        self.banker.shelf(new_score)
+
+        print(f"You have {self.banker.shelved} unbanked points and {6-len(self.dice_deck)} dice remaining")
+
+        return
+
+    def game_cycle(self):
+
+        while True:
+            self.dice_deck = []
+            print(f"Starting round {self.round}")
+            print(f"Rolling {6-len(self.dice_deck)} dice...")
+            if self.roller==None:
+                rolls = self.game.roll_dice(6-len(self.dice_deck))
+            else:
+                rolls = self.roller(6-len(self.dice_deck))
+            print(",".join([str(i) for i in rolls]))
+            score_check = self.game.calculate_score(rolls)
+            if score_check == 0:
+                return self.quit_game()
+            else:
+                dice_to_save = input("Enter dice to keep (no spaces), or (q)uit: ")
                 if dice_to_save == 'q':
-                    quit()
-                    break
+                    return self.quit_game()
                 else:
-                    dice_for_score =[]
-                    for i in range(len(dice_to_save)):
-                        self.dice_deck.append(int(dice_to_save[i]))
-                        dice_for_score.append(int(dice_to_save[i]))
+                    dice_check = [int(elem) for elem in list(dice_to_save)]
+                    result_check = self.cheat_checker(dice_check,list(rolls))
+                    while result_check == False:
+                        print("Cheater!!! Or possibly made a typo...")
+                        print(",".join([str(i) for i in rolls]))
+                        dice_to_save = input("Enter dice to keep (no spaces), or (q)uit: ")
+                        if dice_to_save == 'q':
+                            return self.quit_game()
+                        else:
+                            dice_check = [int(elem) for elem in list(dice_to_save)]
+                            result_check = self.cheat_checker(dice_check,list(rolls))
 
-                    new_score = self.game.calculate_score(dice_for_score)
-
-                    self.banker.shelf(new_score)
-
-                    print(f"You have {self.banker.shelved} unbanked points and {6-len(self.dice_deck)} dice remaining")
+                    self.show_shelf_score(dice_to_save)
 
                     bank_or_not = input("(r)oll again, (b)ank your points or (q)uit ")
+
                     if bank_or_not == 'q':
-                        quit()
-                        break
+                        return self.quit_game()
+
                     elif bank_or_not == 'r':
                         """keep rolling without banking it"""
                         score_return = self.game.calculate_score(rolls)
@@ -149,15 +159,17 @@ class Game:
                             self.round+=1
                             continue
                         else:
-                            quit()
-                            break
+                            return self.quit_game()
                     elif bank_or_not == 'b':
                         """bank the score"""
                         print(f"You banked {self.banker.shelved} points in round {self.round}")
                         self.banker.bank()
-
-                        self.round+=1
                         print(f'Total score is {self.banker.balance} points')
+                        self.round+=1
+
+
+    def play(self):
+        self.greeting()
 
 
 if __name__ == "__main__":
